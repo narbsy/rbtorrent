98f07217e4800fe74d3593034f25862f044f4585
Chris Narburgh <chris.narburgh@gmail.com>
2010-03-23 23:56:51 -0400

    Created/tweaked percentage helper to make pretty bar

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,4 +2,16 @@ helpers do
 	def script(src)
 		haml "%script{:type => 'text/javascript', :src => '#{ src }' }", :layout => false
 	end
+	
+	def percent_bar(percent)
+		if percent.is_a? Float
+			percent = (percent * 100).floor
+		end
+
+		capture_haml do
+			haml_tag :span, { :class => "percent" } do
+				haml_tag :span, "#{percent}%", { :style => "width: #{ percent }%" }
+			end
+		end
+	end
 end
