eecc379763c3552fe0e01b5487b87127c97ff811
Chris Narburgh <chris.narburgh@gmail.com>
2010-03-27 15:51:35 -0400

    Using setInterval and idle-timer to create ajaxy updates for the torrents; modified html files to make this easier, and refactored out some common CSS between forms. Added methods on the backend to help, requiring json as a module.

--- public/application.js
+++ public/application.js
@@ -1,15 +1,63 @@
+var update_interval = undefined;
+
 $(document).ready(function(){
   // Show/hide sibling .torrent
   $('.title').click(function() {
     $(this).siblings('.torrent').slideToggle();
-  })
+  });
+
+  
+  var seconds = 1000;
+  var minutes = 60 * seconds;
+
+  update_interval = setInterval( update_torrents, 10 * seconds );
+  // Idle after 1 minute
+  $.idleTimer( 1 * minutes );
+
+  $(document).bind("idle.idleTimer", function(){
+    clearInterval(update_interval);
+    update_interval = setInterval( update_torrents, 15 * minutes );
+    console.log("setting interval to 15 minutes");
+  });
+  
+  $(document).bind("active.idleTimer", function(){
+    clearInterval(update_interval);
+    update_interval = setInterval( update_torrents, 10 * seconds );
+    console.log("setting interval to 10 seconds");
+  }); 
 });
+
+function update_torrents(){
+  $.get('/update', function(data) {
+    console.log('got: ' + data);
+    console.log('data: ' + data.length);
+    var len = data.length;
+    for(i = 0; i < len; i++) {
+      var update = data[i];
+      var hash = update.hash;
+      console.log(i + ' for: ' + hash );
+      console.log(data.length);
+      // ratio
+      var elem = $('#' + hash + ' .torrent .ratio');
+      $(elem).text('Ratio: ' + (update.ratio / 1000));
+      //percentage
+      elem = $('#' + hash + ' .torrent .percent span');
+      var percent = update.percentage * 100;
+      $(elem).text(percent + '%');
+      $(elem).css('width', percent + '%');
+      //status
+      update_status(hash, update.status);
+    }
+  }, 'json');
+}
 
 function hide_all_but(css_class){
   var css = '.' + css_class
   var li = $('li');
   li.not(css).hide();
   $('li ' + css).show();
+  // which one is faster?
+  // li.filter(css).show();
   return false;
 }
 
@@ -18,12 +66,15 @@ function show_all(){
   return false;
 }
 
+function update_status(hash, new_status){
+  $('#' + hash + ' .title .status').html('[ ' + new_status + ' ]');
+}
+
 function action_with_hash(action, hash){
 	$.post(action, { hash: hash }, 
 		function(data){
 			alert('finished: ' + action + '\n\n\n\n' + data.status);
-      var html = $('#' + hash + ' .title .status').html();
-      $('#' + hash + ' .title .status').html('[ ' + data.status + ' ]');
+      update_status(hash, data.status);
 		}, "json"
 	);
 	return false;
--- rbtorrent.rb
+++ rbtorrent.rb
@@ -6,7 +6,11 @@ Dir["lib/*.rb"].each { |lib| require lib }
 #Load routes in other files
 Dir["routes/*.rb"].each { |route| load route }
 
-setup_download_dir
+setup_download_dir!
+
+before do
+  require_user unless request.path == '/login' || request.path == '/main.css'
+end
 
 get '/' do
   @rtorrent = Rtorrent.new
@@ -25,7 +29,7 @@ post '/add' do
 
   @rtorrent = Rtorrent.new
 
-  name = write_local_file contents
+  name = write_local_file! contents
   unless name
     return haml :add
   end
@@ -48,6 +52,24 @@ end
     content_type :json
     {status: torrent.get_status(true)}.to_json
   end
+end
+
+get '/update' do
+  @rtorrent = Rtorrent.new
+  @list = @rtorrent.list "default", "d.get_hash=", "d.get_name=", "d.get_state=", "d.get_ratio=", "d.get_complete=", "d.is_open=", "d.get_size_chunks=", "d.get_completed_chunks="
+
+  # We're really interested in updating: ratio, percentage & status, so we need to update a couple of properties.
+  list = @rtorrent.list "default", "d.get_hash=", "d.get_state=", "d.get_ratio=", "d.get_complete=", "d.is_open=", "d.get_size_chunks=", "d.get_completed_chunks="
+
+  content_type :json 
+  list.map do |torrent|
+    h = {}
+    [:hash, :status, :ratio].each do |property|
+      h[property] = torrent.send "get_#{property}" 
+    end
+    h[:percentage] = torrent.percentage
+    h
+  end.to_json
 end
 
 #for sass generated css
