8ddcc2b0607906e57ff5a7490c7eb7b2ac853c8c
Chris Narburgh <chris.narburgh@gmail.com>
2010-05-24 17:37:52 -0400

    Modifed response time injector middleware so that it didn't error out on static files

--- lib/response_time_injector.rb
+++ lib/response_time_injector.rb
@@ -1,18 +1,27 @@
+require 'rack'
+
 class Rack::ResponseTimeInjector
   def initialize(app, options = {})
     @app = app
     @format = options[:format] || "%f"
   end
+
+  def returning(value)
+    yield value
+    value
+  end
   
   def call(env)
     t0 = Time.now
-    returning @app.call(env) do |response|
-      response.last.body.gsub! /\$responsetime(?:\((.+)\))?/ do
-        diff = Time.now - t0
-        if @format.respond_to? :call
-          @format.call(diff)
-        else
-          ($1 || @format) % diff
+    returning @app.call(env) do |status, headers, body|
+      if body.kind_of? Array
+        body.last.gsub! /\$responsetime(?:\((.+)\))?/ do
+          diff = Time.now - t0
+          if @format.respond_to? :call
+            @format.call(diff)
+          else
+            ($1 || @format) % diff
+          end
         end
       end
     end
